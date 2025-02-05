44bcda02b337ada8ab5e77d2ff1d55ebd756ec26
MichelC345 <michel.caesarr@gmail.com>
2025-02-05 17:06:13 -0300

    Configuração do ambiente.

---
+++ src/testes/Dollar.java
@@ -0,0 +1,11 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Dollar {
+	int amount = 10;
+    Dollar(int amount) {}			
+    void times(int multiplier) {}
+}
---
+++ src/testes/DollarTest.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DollarTest {
+	@Test
+	public void testMultiplication() {
+       Dollar five = new Dollar(5);
+       five.times(2);
+       assertEquals(10, five.amount);
+    }
+}
